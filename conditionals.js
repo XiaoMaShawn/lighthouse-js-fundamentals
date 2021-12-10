a0fd5f37809b50449f4a97ac325a9af3768fd569
Xiao Ma <ma2014xiao@hotmail.com>
2021-12-10 11:44:57 -0500

    add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -22,3 +22,12 @@ if (temperature < 0) {
 }
 
 console.log("now you're ready to go outside!")
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("you are eligible to vote.");
+}
